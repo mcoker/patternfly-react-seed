8ba7f0f9afae7eef582e635aabdedaf0c9b9b193
Jenny <32821331+jenny-s51@users.noreply.github.com>
2024-07-25 14:56:01 -0400

    fix(styles): update MUI-theme.css

--- src/app/Settings/General/GeneralSettings.tsx
+++ src/app/Settings/General/GeneralSettings.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
-import { Alert, Button, PageSection, Text, TextContent } from '@patternfly/react-core';
+import { Alert as PFAlert, Button as PFButton, PageSection, Text, TextContent } from '@patternfly/react-core';
 import '../../MUI-theme.css';
+import {getTheme} from '../../MUI-theme.tsx';
 
-// import {MUIWarningIcon} from '../General/icons/warning_amber.svg'
+
+import Alert from '@mui/material/Alert';
+import Button from '@mui/material/Button';
+
+import WarningAmberIcon from '@mui/icons-material/WarningAmber';
+
+getTheme();
 
 const MUIThemePOC: React.FunctionComponent = () => (
   <>
@@ -10,15 +17,21 @@ const MUIThemePOC: React.FunctionComponent = () => (
       <TextContent>
         <Text component="h1">Alert demo </Text>
         <Text component="p">
-          Alert theming demo. This is a small demo loading test PF6 component Create stylesheet containing themed tokens
-          for PF6 component, load into demo.
+          Alert theming demo. This is a small demo to test theming the PF6 Alert component. Stylesheet contains themed tokens
+          for PF6 component and has been loaded into this demo.
         </Text>
       </TextContent>
       <br />
-      <Alert
-        // customIcon={<MUIWarningIcon />}
+      MUI Alert:
+      <Alert variant="outlined" severity="warning">
+        This is an outlined warning Alert.
+      </Alert>
+      <br />
+      MUI-themed PF Alert:
+      <PFAlert
+        customIcon={<WarningAmberIcon />}
         variant="warning"
-        title="Warning alert title"
+        title="This is an outlined warning Alert."
         ouiaId="WarningAlert"
       />
     </PageSection>
@@ -26,12 +39,21 @@ const MUIThemePOC: React.FunctionComponent = () => (
       <TextContent>
         <Text component="h1">Button demo </Text>
         <Text component="p">
-          Button theming demo. This is a small demo loading test PF6 component Create stylesheet containing themed
-          tokens for PF6 component, load into demo.
+          Button theming demo. This is a small demo to test theming the PF6 Button component. Stylesheet contains themed
+          tokens for PF6 components and has been loaded into this demo.
         </Text>
         <br />
       </TextContent>
-      <Button onClick={() => console.log('success')}>Button</Button>
+      MUI Button:
+      <br />
+      <Button variant="contained" disableElevation>
+        Button
+      </Button>
+      <br />
+      <br />
+      MUI-themed PF button:
+      <br />
+      <PFButton onClick={() => console.log('success')}>Button</PFButton>
     </PageSection>
   </>
 );
